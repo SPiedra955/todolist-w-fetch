801cc6d00033b0eabad21b80335ecd9f7d935ddf
SPiedra955 <spiedramite@cifpfbmoll.eu>
2026-03-28 23:22:39 +0100

    adding field for user registration

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,146 +1,177 @@
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faCheck,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 //create your first component
 const Home = () => {
+  const url = "https://playground.4geeks.com/todo";
+  const [myUser, setMyUser] = useState("");
+  const [data, setData] = useState({});
+  useEffect(() => {
+    getMyUser();
+  }, []);
+  const [newTask, setNewTask] = useState("");
+  const [error, setError] = useState("");
 
-	const url = 'https://playground.4geeks.com/todo'
-	const [myUser, setMyUser] = useState('Samu')
-	const [data, setData] = useState({})
-	useEffect(() => { getMyUser() }, [])
-	const [newTask, setNewTask] = useState('')
-	const [error, setError] = useState('')
+  const getMyUser = async () => {
+    try {
+      const resp = await fetch(`${url}/users/${myUser}`);
+      if (resp.status === 404) return createUser();
+      if (!resp.ok) throw new Error("Error en GET");
+      const userData = await resp.json();
+      console.log(userData);
+      setData(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const createUser = async (e) => {
+    e.preventDefault();
+    try {
+      if (myUser.trim().length == 0)
+        return setError("No puede estar vacía el campo usuario");
+      if (myUser.trim().length < 3)
+        return setError("Tiene que ser superior a 3 caracteres");
+      const resp = await fetch(`${url}/users/${myUser}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (resp.ok) {
+        setError("");
+        setMyUser("");
+        return getMyUser();
+      }
+      throw new Error("Error en método POST");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const getMyUser = async () => {
-		try {
-			const resp = await fetch(`${url}/users/${myUser}`)
-			if (resp.status === 404) return createUser()
-			if (!resp.ok) throw new Error('Error en GET')
-			const userData = await resp.json()
-			console.log(userData);
-			setData(userData)
-		} catch (error) {
-			console.log(error)
-		}
-	}
+  const createTodos = async (e) => {
+    e.preventDefault();
+    try {
+      if (newTask.trim().length === 0)
+        return setError("No puede estar vacía la tarea");
+      if (newTask.trim().length < 3)
+        return setError("Tiene que ser superior a 3 caracteres");
+      const formattedData = {
+        label: newTask.trim(),
+        is_done: false,
+      };
+      const resp = await fetch(`${url}/todos/${myUser}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formattedData),
+      });
+      if (!resp.ok) throw new Error("Request not ok");
+      setNewTask("");
+      return getMyUser();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const createUser = async () => {
-		try {
-			const resp = await fetch(`${url}/users/${myUser}`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			})
-			if (resp.ok) return getMyUser();
-			throw new Error('Error en método POST')
-		} catch (error) {
-			console.log(error)
-		}
+  const deleteTodo = async (id) => {
+    try {
+      const resp = await fetch(url + "/todos/" + id, {
+        method: "DELETE",
+      });
+      if (!resp.ok) throw new Error("Error deleting ");
+      return getMyUser();
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
 
-	}
+  const updateTodos = async ({ id, label, is_done }) => {
+    try {
+      const formattedData = {
+        label,
+        is_done: !is_done,
+      };
+      const resp = await fetch(url + "/todos/" + id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formattedData),
+      });
+      if (!resp.ok) throw new Error("Error completing task");
+      return getMyUser();
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
 
-	const createTodos = async (e) => {
-		e.preventDefault();
-		try {
-			if (newTask.trim().length === 0) return setError('No puede estar vacía la tarea')
-			if (newTask.trim().length < 3) return setError('Tiene que ser superior a 3 caracteres')
-			const formattedData = {
-				label: newTask.trim(),
-				is_done: false
-			}
-			const resp = await fetch(`${url}/todos/${myUser}`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(formattedData)
-			})
-			if (!resp.ok) throw new Error('Request not ok');
-			setNewTask('')
-			return getMyUser()
-		} catch (error) {
-			console.log(error)
-		}
-	}
+  // Quitar el error del estado
+  useEffect(() => {
+    if (error != "") {
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  }, [error]);
 
-	const deleteTodo = async (id) => {
-		try {
-			const resp = await fetch(url + '/todos/' + id, {
-				method: "DELETE"
-			})
-			if (!resp.ok) throw new Error('Error deleting ')
-			return getMyUser()
-		} catch (error) {
-			console.log(error)
-			setError(error)
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">
+        {data.name
+          ? `Lista de tareas de ${data.name}`
+          : `Crea un usuario para crear lista TODOS`}
+      </h1>
 
-		}
-	}
+      {error && <div className="bg-danger"> {error}</div>}
 
-	const updateTodos = async ({ id, label, is_done }) => {
-		try {
-			const formattedData = {
-				label,
-				is_done: !is_done
-			}
-			const resp = await fetch(url + '/todos/' + id, {
-				method: "PUT",
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(formattedData)
-			})
-			if (!resp.ok) throw new Error('Error completing task')
-			return getMyUser()
-		} catch (error) {
-			console.log(error)
-			setError(error)
-		}
-	}
+      {data.name ? (
+        <form onSubmit={createTodos}>
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <input type="submit" />
+        </form>
+      ) : (
+        <form onSubmit={createUser}>
+          <input
+            type="text"
+            value={myUser}
+            onChange={(e) => setMyUser(e.target.value)}
+          />
+          <input type="submit" value="Crear usuario" />
+        </form>
+      )}
 
-	// Quitar el error del estado
-	useEffect(() => {
-		if (error != '') {
-			setTimeout(() => {
-				setError('')
-			}, 3000)
-		}
-	}, [error])
+      <ul>
+        {data.todos?.map((el) => (
+          <li key={el.id} className={el.is_done ? " bg-success" : "border"}>
+            {el.label}
+            <span onClick={() => updateTodos(el)}>
+              <FontAwesomeIcon
+                className="mx-3"
+                icon={el.is_done ? faCircleXmark : faCheck}
+              />
+            </span>
 
-	return (
-		<div className="text-center">
-
-			<h1 className="text-center mt-5">Lista de tareas de {data.name}</h1>
-
-			{error && <div className="bg-danger"> {error}</div>}
-
-			<form onSubmit={createTodos} >
-				<input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
-				<input type="submit" />
-			</form>
-
-			<ul>
-
-				{data.todos?.map(el => <li key={el.id} className={el.is_done ? ' bg-success' : 'border'}>
-					{el.label}
-					<span onClick={() => updateTodos(el)}>
-						<FontAwesomeIcon
-							className="mx-3"
-							icon={el.is_done ? faCircleXmark : faCheck}
-						/>
-					</span>
-
-					<span onClick={() => deleteTodo(el.id)}>
-						<FontAwesomeIcon icon={faTrash} />
-					</span>
-				</li>)}
-			</ul>
-
-		</div>
-	);
+            <span onClick={() => deleteTodo(el.id)}>
+              <FontAwesomeIcon icon={faTrash} />
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
